1e0f0e65b9cd80a38a82e17c0da64174a14cf039
Yash Vardhan Srivastava <y.srivastava@iitg.ac.in>
2024-10-05 12:51:28 +0530

    day3

---
+++ not_a_subset_sum.cpp
@@ -0,0 +1,8 @@
+long long findSmallest(vector<int> &arr) {
+        long long curr = 1;
+        for(auto it:arr){
+            if(curr<it) return curr;
+             curr+=it;
+        }
+        return curr;
+}
