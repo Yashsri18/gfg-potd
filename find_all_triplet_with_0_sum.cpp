a1b6a9707040fd1e0cb90ac65b17001fe5f5c29b
Yash Vardhan Srivastava <y.srivastava@iitg.ac.in>
2024-11-04 11:07:46 +0530

    day 32

---
+++ find_all_triplet_with_0_sum.cpp
@@ -0,0 +1,24 @@
+// User function Template for C++
+class Solution {
+  public:
+    vector<vector<int>> findTriplets(vector<int> &arr) {
+        // Code here
+               int n = arr.size() ;
+        
+        unordered_map<int , vector<int>> mp ;
+        for(int i = 0 ; i < n ; i ++) mp[arr[i]].push_back(i) ;
+        
+        vector<vector<int>> ans ;
+        
+        for(int i = 1 ; i < n ; i ++){
+            for(int j = i + 1 ; j < n ; j ++ ){
+                int sum = -1*(arr[i] + arr[j]) ;
+                for( int k : mp[sum]){
+                    if(k < i)ans.push_back({k , i , j}) ;
+                }
+            }
+        }
+        
+        return ans ; 
+    }
+};
