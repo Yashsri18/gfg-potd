14f5025c6f3308f23d063e1362f80e38600eb506
Yash Vardhan Srivastava <y.srivastava@iitg.ac.in>
2024-11-16 11:53:37 +0530

    day41

---
+++ Move_all_zero_to_end.cpp
@@ -0,0 +1,14 @@
+void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int count = 0;
+        int n = arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i] == 0){
+                count++;
+            }
+        }
+        arr.erase((remove(arr.begin(),arr.end(),0)),arr.end());
+        for(int i=1;i<=count;i++){
+            arr.push_back(0);
+        }
+    }
